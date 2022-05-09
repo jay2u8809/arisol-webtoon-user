d561cafba33ef14dbab4a855cefdf701e027676b
Jung Ian <jay2u8809@gmail.com>
2022-05-09 23:05:13 +0900

    Feat. init project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# arisol-webtoon-user
-webtoon user service
+# Arisol Webtoon Project - User Service
+
+## Spec
+- P/L: Kotlin (java 11)
+- Framework: SpringBoot 2.7.0 (RC1)
---
+++ src/test/kotlin/com/arisol/webtoon/user/UserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arisol.webtoon.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
